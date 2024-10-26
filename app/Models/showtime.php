01106ca2e14d5ea9c1ac4687da63da0356a9b488
moi3e <dongt20044@gmail.com>
2024-10-26 22:25:40 +0700

    Fix

--- app/Models/showtime.php
+++ app/Models/showtime.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class showtime extends Model
+class Showtime extends Model
 {
     use HasFactory;
 
